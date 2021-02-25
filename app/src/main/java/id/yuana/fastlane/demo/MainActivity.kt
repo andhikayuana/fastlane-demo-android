1d363e7d887346e881db4ca79691d26aca45c034
ANDHIKA YUANA <andhikayuana@gmail.com>
2021-02-26 05:54:14 +0700

    init :rocket:

---
+++ app/src/main/java/id/yuana/fastlane/demo/MainActivity.kt
@@ -0,0 +1,17 @@
+package id.yuana.fastlane.demo
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Log.d("YUANA", "Hello from android")
+        }
+    }
+}
